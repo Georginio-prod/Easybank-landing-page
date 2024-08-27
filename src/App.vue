24d6682adfa85995ee4a6b13d3029e9aabbd8c78
Georginio-prod <etonameklou6@gmail.com>
2024-08-27 10:39:37 +0000

    style: add navbar for component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
-<script>
-
+<script setup>
+import navbar from './components/navbar.vue'
 </script>
 
 
 <template>
-  <div>
-    
-  </div>
+ 
+    <navbar/>
+  
 </template>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,7 +1,24 @@
 <template>
-    <div>
-        
+    <div class="w-full h-auto flex pt-5 px-40">
+        <div class="w-full flex justify-between items-center">
+            <img src="/public/logo.svg" alt="logo">
+
+            <div class="flex gap-5 justify-center items-center">
+                <button>Home</button>
+                <button>About</button>
+                <button>Contact</button>
+                <button>Blog</button>
+                <button>Careers</button>
+            </div>
+
+            <div>
+                <button>
+                    Request Invite
+                </button>
+            </div>
+        </div>
     </div>
+
 </template>
 
 <script>

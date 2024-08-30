301c96371ad46977d174cfd8600e5860c0e2f966
Georginio-prod <etonameklou6@gmail.com>
2024-08-30 16:40:11 +0000

    style: add the hover

--- src/components/ft.vue
+++ src/components/ft.vue
@@ -5,31 +5,31 @@
         <div class="flex flex-col items-center lg:items-start gap-14 lg:pr-36">
           <img src="/public/Group 9.png" alt="Logo" class="mx-auto lg:mx-0">
           <div class="flex gap-3">
-            <img src="/public/icon-facebook.svg" alt="Facebook">
-            <img src="/public/icon-youtube.svg" alt="YouTube">
-            <img src="/public/icon-twitter.svg" alt="Twitter">
-            <img src="/public/icon-pinterest.svg" alt="Pinterest">
-            <img src="/public/icon-instagram.svg" alt="Instagram">
+            <img src="/public/icon-facebook.svg" alt="Facebook" class="hover:scale-110 hover: transition duration-300 ease-in-out">
+            <img src="/public/icon-youtube.svg" alt="YouTube" class="hover:scale-110 hover: transition duration-300 ease-in-out">
+            <img src="/public/icon-twitter.svg" alt="Twitter" class="hover:scale-110 hover: transition duration-300 ease-in-out">
+            <img src="/public/icon-pinterest.svg" alt="Pinterest" class="hover:scale-110 hover: transition duration-300 ease-in-out">
+            <img src="/public/icon-instagram.svg" alt="Instagram" class="hover:scale-110 hover: transition duration-300 ease-in-out">
           </div>
         </div>
   
         <!-- Sections de liens -->
         <div class="flex flex-col lg:flex-row justify-between lg:justify-start lg:items-start lg:space-x-28 text-center lg:text-left text-[15px] tracking-[-0.23px] leading-[26px]">
           <div class="flex flex-col gap-2 lg:items-start">
-            <button>About Us</button>
-            <button>Contact</button>
-            <button>Blog</button>
+            <button class="hover:text-lin">About Us</button>
+            <button class="hover:text-lin">Contact</button>
+            <button class="hover:text-lin">Blog</button>
           </div>
           <div class="flex flex-col gap-2 mt-4 lg:mt-0 lg:items-start">
-            <button>Careers</button>
-            <button>Support</button>
-            <button>Privacy Policy</button>
+            <button class="hover:text-lin">Careers</button>
+            <button class="hover:text-lin">Support</button>
+            <button class="hover:text-lin">Privacy Policy</button>
           </div>
         </div>
   
         <!-- Section bouton et copyright -->
         <div class="flex flex-col items-center lg:ml-auto text-center lg:text-right mt-6 lg:mt-0">
-          <button class="text-white bg-gradient-to-br from-lin to-lin2 px-8 py-3 rounded-full text-sm font-bold lg:ml-16">
+          <button class="text-white bg-gradient-to-br from-lin to-lin2 px-8 py-3 rounded-full text-sm font-bold lg:ml-16 transition-opacity hover:opacity-40">
             Request Invite
           </button>
           <p class="opacity-50 mt-4 lg:mt-6 text-[15px] tracking-[-0.23px]">© Easybank. All Rights Reserved</p>
